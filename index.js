90c62fa375c94e1727c415e4d7506d3ae5d26140
jrrdl1506 <128421660+jrrdl1506@users.noreply.github.com>
2024-03-04 11:24:28 -0600

    cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const conectarDB = require('./config/db');
 const app = express();
+const cors = require("cors");
 
 conectarDB();
+app.use(cors());
 app.use(express.json());
 
 
